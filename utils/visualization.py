e44e23ed17540be39da7b120aba89f5b2b67d8c8
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-02-29 17:03:12 +0100

    Added support to visualize list of PIL images

--- utils/visualization.py
+++ utils/visualization.py
@@ -3,37 +3,46 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import random
 
-def visualize_n_images(image_dir, rows=1, cols=5, use_random=True):
+
+def visualize_n_images(images=None, image_dir=None, rows=1, cols=5, use_random=True):
     """
-    Visualizes n images located in given image directory
+    Visualizes n images either located in given image directory, 
+    or in given list of PIL images
     
+    Precondition: Can only pass either images or image_dir
+
     Parameters:
+    - images: Batch of images to visualize
     - image_dir: Directory that contains the images.
     - rows, cols: The layout of the subplot grid.
-    - use_random: If false it uses the first rows * cols \\
+    - use_random: If false it uses the first rows * cols 
               images located in image_dir.
     """
+    assert (images is None) != (image_dir is None), "Can only pass either images or image_dir, not both"
 
-    file_paths = []
-    for root, dirs, files in os.walk(image_dir):
-        for file in files:
-            if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # Add or remove file formats as needed
-                file_path = os.path.join(root, file)
-                file_paths.append(file_path)
+    if image_dir:
+        if not os.path.exists(image_dir):
+            raise FileNotFoundError(f"Directory '{image_dir}' does not exist.")
 
-    num_images = rows * cols
-    if use_random and len(file_paths) >= num_images:
-        # Randomly select `num_images` from the list of file paths
-        selected_paths = random.sample(file_paths, num_images)
+        file_paths = [os.path.join(root, file) for root, _, files in os.walk(image_dir) for file in files
+                      if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+        if not file_paths:
+            raise ValueError(f"No images found in '{image_dir}'.")
+
+        num_images = rows * cols
+        if use_random and len(file_paths) >= num_images:
+            selected_paths = random.sample(file_paths, num_images)
+        else:
+            selected_paths = file_paths[:num_images]
+        visual_imgs = [Image.open(img_path) for img_path in selected_paths]
     else:
-        # If not enough images or `use_random` is False, take the first `num_images`
-        selected_paths = file_paths[:num_images]
+        visual_imgs = images
 
     fig, axes = plt.subplots(rows, cols, figsize=(20, 4))
     axes = axes.flatten()
-
-    for img_path, ax in zip(selected_paths, axes):
-        img = Image.open(img_path)
+    
+    for img, ax in zip(visual_imgs, axes):
         ax.imshow(img)
         ax.axis('off')
 
