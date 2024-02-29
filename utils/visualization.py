856a5a09b8fda7eda9130bd83aa073653cc27c44
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-02-29 03:40:09 +0100

    Fixed bug

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 from PIL import Image
+import random
 
 def visualize_n_images(image_dir, rows=1, cols=5, random=True):
     """
@@ -13,40 +14,28 @@ def visualize_n_images(image_dir, rows=1, cols=5, random=True):
               images located in image_dir.
     """
 
+    file_paths = []
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # Add or remove file formats as needed
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+
+    num_images = rows * cols
+    if random and len(file_paths) >= num_images:
+        # Randomly select `num_images` from the list of file paths
+        selected_paths = random.sample(file_paths, num_images)
+    else:
+        # If not enough images or `use_random` is False, take the first `num_images`
+        selected_paths = file_paths[:num_images]
+
     fig, axes = plt.subplots(rows, cols, figsize=(20, 4))
     axes = axes.flatten()
 
-    num_images = rows * cols
-    image_paths = []
-    if random:
-        file_paths = []
-        for root, dirs, files in os.walk(image_dir):
-               for file in files:
-                   if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # Add or remove file formats as needed
-                       # Construct the path to the image
-                       file_path = os.path.join(root, file)
-                       file_paths.append(file_path)
-        indices = random.randrange(num_images)
- 
-        image_paths = file_paths[indices]                       
+    for img_path, ax in zip(selected_paths, axes):
+        img = Image.open(img_path)
+        ax.imshow(img)
+        ax.axis('off')
 
-    else:
-        for root, dirs, files in os.walk(image_dir):
-            for file in files:
-                if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # Add or remove file formats as needed
-                    # Construct the path to the image
-                    file_path = os.path.join(root, file)
-                    image_paths.append(file_path)
-                    if(len(image_paths) == num_images):
-                        break
-    
-    for img_path, ax in zip(image_paths, axes):
-        with Image.open(img_path) as img:
-            if isinstance(img, Image.Image):
-                ax.imshow(img)
-            else:
-                ax.imshow(img, cmap='gray')  # For grayscale images
-            ax.axis('off')
-    
     plt.tight_layout()
     plt.show()
